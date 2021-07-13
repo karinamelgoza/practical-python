7a5e4628d5c24dca56aa091cc4d7538f7910f15f
Karina Melgoza <kmelgoza08@gmail.com>
2021-07-13 16:14:08 -0700

    add part 2

---
+++ game.py
@@ -0,0 +1,25 @@
+import random
+
+player_name = input("What's your name?\n")
+print(player_name, ', I am thinking of a number between 1 and 100.', sep='')
+print('Try to guess my number.')
+comp_num = random.randint(1, 100)
+count = 1
+
+
+def game():
+    guess = int(input('Your guess?\n'))
+    global count
+    if guess > comp_num:
+        print('Your guess to too high. Try again.')
+        count += 1
+        game()
+    elif guess < comp_num:
+        print('Your guess to too low. Try again.')
+        count += 1
+        game()
+    else:
+        print("That's right! You found my number in", count, 'tries')
+
+
+game()
