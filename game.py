ddf89f75523bd3244be0ed65159855dd3121f83e
Karina Melgoza <kmelgoza08@gmail.com>
2021-07-13 19:51:40 -0700

    add while loop

--- game.py
+++ game.py
@@ -6,20 +6,34 @@ print('Try to guess my number.')
 comp_num = random.randint(1, 100)
 count = 1
 
+# def game():
+#     guess = int(input('Your guess?\n'))
+#     global count
+#     if guess > comp_num:
+#         print('Your guess to too high. Try again.')
+#         count += 1
+#         game()
+#     elif guess < comp_num:
+#         print('Your guess to too low. Try again.')
+#         count += 1
+#         game()
+#     else:
+#         print("That's right! You found my number in", count, 'tries')
 
-def game():
-    guess = int(input('Your guess?\n'))
-    global count
+
+# game()
+
+guess = int(input('Your guess?\n'))
+
+while guess != comp_num:
     if guess > comp_num:
         print('Your guess to too high. Try again.')
         count += 1
-        game()
+        guess = int(input('Your guess?\n'))
+
     elif guess < comp_num:
         print('Your guess to too low. Try again.')
         count += 1
-        game()
-    else:
-        print("That's right! You found my number in", count, 'tries')
-
-
-game()
+        guess = int(input('Your guess?\n'))
+else:
+    print("That's right! You found my number in", count, 'tries')
